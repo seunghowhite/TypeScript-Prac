1ae5bf66d0412d8d812cad3f325e9df939b54b9e
seunghowhite <105100315+seunghowhite@users.noreply.github.com>
2023-05-10 18:15:31 +0900

    [firstTry] study6.  literal type 적용하기. as const의 효능

--- src/study/Study6.tsx
+++ src/study/Study6.tsx
@@ -7,7 +7,20 @@ function Study6() {
   name = "beack";
 
   let me: "tall" | "solo";
-  me = "solo";
+  me = "solo"; //이렇게 가능하다이말이지
+
+  //함수 작성할때 좋을꺼같다.
+  //litertype은 const상위호완
+  var 자료 = {
+    name: "kim",
+  } as const; //이건뭐냐?
+  //!이 object는 literal type으로 바꿔달라.
+  //결론적으로 자료는 kim타입이다.
+  //2번째 기능으로 readonly만 가능하다
+
+  function 내함수(a: "kim") {}
+  내함수(자료.name);
+
   return (
     <div>
       <span>공부날짜:5월10일</span>

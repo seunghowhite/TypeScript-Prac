4548553339e3988b87ec8b3a0f4bf43adbffdb5a
seunghowhite <105100315+seunghowhite@users.noreply.github.com>
2023-05-15 14:20:10 +0900

    [firstTry] d.ts 사용해 보기

---
+++ src/study/MoreStudy9.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { aget } from "../types/MoreStudy9";
+
+function MoreStudy9() {
+  let age: aget = 1;
+  return <div>MoreStudy9</div>;
+}
+
+export default MoreStudy9;
